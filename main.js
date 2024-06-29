f2fe4e9135a59ad7539834ff2611ba7c59e4077a
Sergey Gusterev <copywriter-sg@yandex.ru>
2024-06-29 06:22:01 +0300

    bugfix: header menu

--- main.js
+++ main.js
@@ -2,24 +2,14 @@ import 'normalize.css';
 import './assets/styles/main.css';
 
 const burger = document.querySelector('.header__burger');
-
-const headerNavigation = document.querySelector('.header__nav');
-const headerContacts = document.querySelector('.header__top-contacts');
+const menu = document.querySelector('.header__menu');
 
 const toggledMenu = () => {
   burger.classList.toggle('header__burger_active');
   const isOpenBurger = burger.classList.contains('header__burger_active');
   burger.ariaLabel = isOpenBurger ? 'Закрыть меню' : 'Открыть меню';
 
-  headerNavigation.classList.toggle('header__nav_open');
-  headerContacts.classList.toggle('header__top-contacts_open');
-
-  const positionInfoHeaderNavigation = headerNavigation.getBoundingClientRect();
-  const heightHeaderNavigation = positionInfoHeaderNavigation.height;
-
-  headerContacts.style.top = isOpenBurger
-    ? 70 + heightHeaderNavigation + 'px'
-    : 70 + 'px';
+  menu.classList.toggle('header__menu_open');
 };
 
 burger.addEventListener('click', toggledMenu);
