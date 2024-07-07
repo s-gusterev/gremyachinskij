69bd1911be291fe61cab6a9f1119725001c13276
Sergey Gusterev <copywriter-sg@yandex.ru>
2024-07-07 05:44:18 +0300

    feat: products slider

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 import 'normalize.css';
 import './assets/styles/main.css';
 
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
+const productsSlider = document.querySelector('.products__slider');
+const productsList = document.querySelector('.products__list');
+const productsSlide = document.querySelectorAll('.products__slide');
 
 const toggledMenu = () => {
   burger.classList.toggle('header__burger_active');
@@ -13,3 +22,53 @@ const toggledMenu = () => {
 };
 
 burger.addEventListener('click', toggledMenu);
+
+const breakpoint = window.matchMedia('(min-width: 824px)');
+
+let productsSwiper;
+
+const breakpointChecker = function () {
+  if (breakpoint.matches === true) {
+    if (productsSwiper !== undefined) {
+      productsSwiper.destroy(true, true);
+      productsSlider.classList.remove('swiper');
+      productsList.classList.remove('swiper-wrapper');
+      productsSlide.forEach((slide) => {
+        slide.classList.remove('swiper-slide');
+      });
+    }
+    return;
+  } else if (breakpoint.matches === false) {
+    productsSlider.classList.add('swiper');
+    productsList.classList.add('swiper-wrapper');
+    productsSlide.forEach((slide) => {
+      slide.classList.add('swiper-slide');
+      slide.style.width = '360px';
+    });
+    return enableProductsSwiper();
+  }
+};
+
+const enableProductsSwiper = function () {
+  productsSwiper = new Swiper('.swiper', {
+    slidesPerGroup: 1,
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 40,
+    watchOverflow: true,
+    modules: [Navigation],
+    navigation: {
+      prevEl: '.products__button-slider_prev',
+      nextEl: '.products__button-slider_next',
+    },
+    on: {
+      resize: function () {
+        productsSwiper.update();
+      },
+    },
+  });
+};
+
+breakpoint.addEventListener('change', () => breakpointChecker());
+
+breakpointChecker();
